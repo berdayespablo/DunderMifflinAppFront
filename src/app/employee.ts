d2bffd9f0a034cd7dd1eadafa05baea570c5c56b
Pablo <berdayespablo@gmail.com>
2022-09-26 16:34:51 +0200

    Creacion del servicio de empleado

---
+++ src/app/employee.ts
@@ -0,0 +1,9 @@
+export interface Employee{
+    id: number;
+    name: string;
+    jobTitle: string;
+    performer: string;
+    quote: string;
+    imageUrl: string;
+    employeeCode: string;
+}
